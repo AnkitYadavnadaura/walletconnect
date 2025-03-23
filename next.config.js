1a14a4fe101a654fc08e6d1a20ddd889b018349b
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2025-03-23 15:47:47 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
+  distDir: "build",
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+
     return config;
-  }
+  },
 };
 
 module.exports = nextConfig;
